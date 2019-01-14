13eec10367e4096733c5178ed93916d1aa09d9c6
Aditi3 <aditi.gvc@gmail.com>
2019-01-14 12:14:34 +0530

    update in-line comments & cleanup

--- CleverTapSDK/CleverTap+Inbox.h
+++ CleverTapSDK/CleverTap+Inbox.h
@@ -2,6 +2,12 @@
 #import "CleverTap.h"
 @class CleverTapInboxMessageContent;
 
+/*!
+ 
+ @abstract
+ The `CleverTapInboxMessage` represents the inbox message object.
+ */
+
 @interface CleverTapInboxMessage : NSObject
 
 @property (nullable, nonatomic, copy, readonly) NSDictionary *json;
@@ -23,6 +29,12 @@
 - (void)setRead:(BOOL)read;
 
 @end
+
+/*!
+ 
+ @abstract
+  The `CleverTapInboxMessageContent` represents the inbox message content.
+ */
 
 @interface CleverTapInboxMessageContent : NSObject
 
@@ -50,6 +62,12 @@
 - (void)messageDidSelect:(CleverTapInboxMessage *)message atIndex:(int)index withButtonIndex:(int)buttonIndex;
 @end
 
+/*!
+ 
+ @abstract
+ The `CleverTapInboxStyleConfig` has all the parameters required to configure the styling of your Inbox ViewController
+ */
+
 @interface CleverTapInboxStyleConfig : NSObject
 
 @property (nonatomic, strong) NSString *title;
@@ -74,23 +92,104 @@ typedef void (^CleverTapInboxUpdatedBlock)(void);
 
 @interface CleverTap (Inbox)
 
+/*!
+ @method
+ 
+ @abstract
+ Initialized the inbox controller and sends a callback.
+ 
+ @discussion
+ Use this method to initialize the inbox controller for the user.
+ You must call this method separately for each instance of CleverTap.
+ */
+
 - (void)initializeInboxWithCallback:(CleverTapInboxSuccessBlock _Nonnull)callback;
+
+/*!
+ @method
+ 
+ @abstract
+ This method returns the total number of inbox messages for the user.
+ */
 
 - (NSUInteger)getInboxMessageCount;
 
+/*!
+ @method
+ 
+ @abstract
+ This method returns the total number of unread inbox messages for the user.
+ */
+
 - (NSUInteger)getInboxMessageUnreadCount;
+
+/*!
+ @method
+ Get all the inbox messages.
+ 
+ @abstract
+ This method returns an array of `CleverTapInboxMessage` objects for the user.
+ */
 
 - (NSArray<CleverTapInboxMessage *> * _Nonnull )getAllInboxMessages;
 
+/*!
+ @method
+ Get all the unread inbox messages.
+ 
+ @abstract
+ This method returns an array of unread `CleverTapInboxMessage` objects for the user.
+ */
+
 - (NSArray<CleverTapInboxMessage *> * _Nonnull )getUnreadInboxMessages;
+
+/*!
+ @method
+ 
+ @abstract
+ This method returns `CleverTapInboxMessage` object that belongs to the given messageId.
+ */
 
 - (CleverTapInboxMessage * _Nullable )getInboxMessageForId:(NSString *)messageId;
 
+/*!
+ @method
+ 
+ @abstract
+ This method deletes the given `CleverTapInboxMessage` object.
+ */
+
 - (void)deleteInboxMessage:(CleverTapInboxMessage * _Nonnull )message;
+
+/*!
+ @method
+ 
+ @abstract
+ This method marks the given `CleverTapInboxMessage` object as read.
+ */
 
 - (void)markReadInboxMessage:(CleverTapInboxMessage * _Nonnull) message;
 
+/*!
+ @method
+ 
+ @abstract
+ Register a callback block when inbox messages are updated.
+ */
+
 - (void)registerInboxUpdatedBlock:(CleverTapInboxUpdatedBlock _Nonnull)block;
+
+/**
+ 
+ @method
+ This method opens the controller to display the inbox messages.
+ 
+ @abstract
+ The `CleverTapInboxViewControllerDelegate` protocol provides a method for notifying
+ your application when a inbox message is clicked (or tapped).
+ 
+ The `CleverTapInboxStyleConfig` has all the parameters required to configure the styling of your Inbox ViewController
+ */
 
 - (CleverTapInboxViewController * _Nullable)newInboxViewControllerWithConfig:(CleverTapInboxStyleConfig * _Nullable )config andDelegate:(id<CleverTapInboxViewControllerDelegate> _Nullable )delegate;
 
