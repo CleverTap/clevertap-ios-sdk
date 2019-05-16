89624fe5b525622f554ad41a936b9c0d6404efcd
Aditi3 <aditi.gvc@gmail.com>
2019-05-16 13:41:08 +0530

    update Objc starter

---
+++ ObjCStarter/ObjCStarter/CTWebViewVC.h
@@ -0,0 +1,17 @@
+//
+//  CTWebViewVC.h
+//  ObjCStarter
+//
+//  Created by Aditi Agrawal on 16/05/19.
+//  Copyright © 2019 Aditi Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTWebViewVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- test/CleverTapSDKTest/CleverTapSDKTest/AppDelegate.swift
+++ test/CleverTapSDKTest/CleverTapSDKTest/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             // Fallback on earlier versions
         };
         
-        CleverTap.setCredentialsWithAccountID("TEST-Z9R-486-4W5Z", andToken: "TEST-6b4-2c1")
+        CleverTap.setCredentialsWithAccountID("W9R-486-4W5Z", andToken: "6b4-2c0")
+
+//        CleverTap.setCredentialsWithAccountID("TEST-Z9R-486-4W5Z", andToken: "TEST-6b4-2c1")
         CleverTap.autoIntegrate(withCleverTapID: "Aditi")
         CleverTap.sharedInstance()?.enableDeviceNetworkInfoReporting(true)
         CleverTap.setDebugLevel(3)
