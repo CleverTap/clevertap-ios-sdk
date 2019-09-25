4a73e9f24ce11637b7c6a3ebb248be6e7c358fee
Aditi3 <aditi.gvc@gmail.com>
2019-09-25 10:14:47 +0530

    cleanup

--- test/CTNotificationService/ExampleSwift/Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-
-#import <CleverTapSDK/CleverTap.h>
-#import <CleverTapSDK/CleverTapBuildInfo.h>
-#import <CleverTapSDK/CleverTapEventDetail.h>
-#import <CleverTapSDK/CleverTapSyncDelegate.h>
-#import <CleverTapSDK/CleverTapUTMDetail.h>
-#import <CleverTapSDK/CleverTapInAppNotificationDelegate.h>
