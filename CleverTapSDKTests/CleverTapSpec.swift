2086bc099d9d50710119d1a603379cf86abecd27
Yogesh Singh <yogesh@clevertap.com>
2020-05-24 18:57:39 +0530

    added personalization tests

--- CleverTapSDKTests/CleverTapSpec.swift
+++ CleverTapSDKTests/CleverTapSpec.swift
@@ -10,9 +10,9 @@ class CleverTapSpec: QuickSpec {
         
         describe("a CleverTap instance") {
             
-            context("is initialized") {
-                    
-                it("with shared instance singleton") {
+            context("is initialized with shared instance singleton") {
+                  
+                it("when AccountID and Token are set") {
                     
                     CleverTap.setCredentialsWithAccountID("AccountID", andToken: "Token")
                     
@@ -20,6 +20,43 @@ class CleverTapSpec: QuickSpec {
                     
                     expect(instance).toNot(beNil())
                 }
+                
+                it("when Config object is set") {
+                    
+                    let config = CleverTapInstanceConfig(accountId: "AccountID", accountToken: "Token", accountRegion: "Region")
+                    
+                    let instance = CleverTap.instance(with: config)
+                    
+                    expect(instance).toNot(beNil())
+                }
+            }
+        }
+        
+        describe("a CleverTap class object") {
+            
+            it("sets Account and Token in Plist Info") {
+                CleverTap.setCredentialsWithAccountID("AccountID", andToken: "Token")
+                
+                let plistInfoInstance = CTPlistInfo.sharedInstance()
+                
+                expect(plistInfoInstance?.accountId).to(equal("AccountID"))
+                expect(plistInfoInstance?.accountToken).to(equal("Token"))
+            }
+            
+            it("enables personalization") {
+                CleverTap.enablePersonalization()
+                
+                let value = CTPreferences.getIntForKey("boolPersonalisationEnabled", withResetValue: 0)
+                
+                expect(value).to(beTruthy())
+            }
+            
+            it("disbles personalization") {
+                CleverTap.disablePersonalization()
+                
+                let value = CTPreferences.getIntForKey("boolPersonalisationEnabled", withResetValue: 1)
+                
+                expect(value).toNot(beTruthy())
             }
         }
     }
