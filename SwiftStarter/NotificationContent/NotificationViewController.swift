f7faacf7ba3897e99efd6ab8e6580977a5376fd8
Yogesh Singh <yogesh@clevertap.com>
2020-06-03 18:34:42 +0530

    SDK-6: added notification content extension in swift starter app

---
+++ SwiftStarter/NotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContent
+//
+//  Created by Yogesh Singh on 03/06/20.
+//  Copyright © 2020 CleverTap. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
