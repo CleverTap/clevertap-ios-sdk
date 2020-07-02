cc6457508f06198515964a80c2ab937cc775a76d
Yogesh Singh <yogesh@clevertap.com>
2020-07-02 20:00:31 +0530

    removed Pods/ directory from Git for ObjC & Swift Example apps

--- ObjCStarter/Pods/Target Support Files/Pods-ObjCStarterTests/Pods-ObjCStarterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ObjCStarterTests : NSObject
-@end
-@implementation PodsDummy_Pods_ObjCStarterTests
-@end
--- SwiftStarter/Pods/Target Support Files/CleverTapWatchOS/CleverTapWatchOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CleverTapWatchOS : NSObject
-@end
-@implementation PodsDummy_CleverTapWatchOS
-@end
