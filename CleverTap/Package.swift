9f94e79830476ac90cd4d0a2e3c67c665ce1c311
Yogesh Singh <yogesh@clevertap.com>
2020-10-26 04:09:21 +0530

    SDK-367: added header search paths for files in sub-folders which were not getting imported

--- CleverTap/Package.swift
+++ CleverTap/Package.swift
@@ -20,6 +20,30 @@ let package = Package(
         .target(
             name: "CleverTap",
             dependencies: ["SDWebImage"]
+            cSettings: [
+                .headerSearchPath("CleverTapSDK"),
+                .headerSearchPath("ABTesting"),
+                .headerSearchPath("ABTesting/controllers"),
+                .headerSearchPath("ABTesting/models"), // TODO: added all ABTesting subfolders!
+                .headerSearchPath("DisplayUnit"),
+                .headerSearchPath("DisplayUnit/controllers"),
+                .headerSearchPath("DisplayUnit/models"),
+                .headerSearchPath("FeatureFlags"),
+                .headerSearchPath("FeatureFlags/controllers"),
+                .headerSearchPath("FeatureFlags/models"),
+                .headerSearchPath("InApps"),
+                .headerSearchPath("InApps/images"),
+                .headerSearchPath("Inbox"),
+                .headerSearchPath("Inbox/cells"),
+                .headerSearchPath("Inbox/config"),
+                .headerSearchPath("Inbox/controllers"),
+                .headerSearchPath("Inbox/images"),
+                .headerSearchPath("Inbox/models"),
+                .headerSearchPath("Inbox/views"),
+                .headerSearchPath("ProductConfig"),
+                .headerSearchPath("ProductConfig/models"),
+                .headerSearchPath("ProductConfig/controllers")
+            ],
         ),
         .testTarget(
             name: "CleverTapTests",
