0db4b2c17a871b996ad3c3681a6bbfc9b3d241be
Yogesh Singh <yogesh@clevertap.com>
2020-11-03 15:06:40 +0530

    WIP: build failing

---
+++ SPMStarter/SPMStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMStarter
+//
+//  Created by Yogesh Singh on 02/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
