79435ab32bba33af603d85bcdc241f02daec3f94
Aditi3 <aditi.gvc@gmail.com>
2020-11-04 17:33:26 +0530

    created SPMStarter Project
    SDK-480

---
+++ SPMStarter/SPMStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPMStarter
+//
+//  Created by Aditi Agrawal on 04/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
