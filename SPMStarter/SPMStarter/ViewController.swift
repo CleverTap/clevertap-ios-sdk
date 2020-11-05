b9b862c1c517a886a6f610d69cdb36b4bcaa34b4
Aditi3 <aditi.gvc@gmail.com>
2020-11-05 18:13:42 +0530

    Update SPM Starter App for CT App Inbox
    SDK-480

--- SPMStarter/SPMStarter/ViewController.swift
+++ SPMStarter/SPMStarter/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import CleverTapSDK
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CleverTapInboxViewControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initializeAppInbox()
+        
+        CleverTap.sharedInstance()?.recordEvent("Product Viewed")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0, execute: {
+            self.showAppInbox()
+        })
     }
-
-
+    
+    func initializeAppInbox() {
+        CleverTap.sharedInstance()?.initializeInbox(callback: ({ (success) in
+            let messageCount = CleverTap.sharedInstance()?.getInboxMessageCount()
+            let unreadCount = CleverTap.sharedInstance()?.getInboxMessageUnreadCount()
+            print("Inbox Message:\(String(describing: messageCount))/\(String(describing: unreadCount)) unread")
+        }))
+    }
+    
+    func showAppInbox() {
+        
+        // config the style of App Inbox Controller
+        let style = CleverTapInboxStyleConfig.init()
+        style.title = "App Inbox"
+        if let inboxController = CleverTap.sharedInstance()?.newInboxViewController(with: style, andDelegate: self) {
+            let navigationController = UINavigationController.init(rootViewController: inboxController)
+            self.present(navigationController, animated: true, completion: nil)
+        }
+    }
+    
 }
 
