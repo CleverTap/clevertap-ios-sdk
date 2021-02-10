c327588e39bec7a4a2494cbcdf1309f14821c4ad
Aditi3 <aditi.gvc@gmail.com>
2021-02-10 13:08:22 +0530

    Created ManualInstallation Example Project

---
+++ ManualInstallation/ManualInstallation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualInstallation
+//
+//  Created by Aditi Agrawal on 10/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
