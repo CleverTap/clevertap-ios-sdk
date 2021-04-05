e34290ec12a691b63224494e04557330a772ba31
Aditi3 <aditi.gvc@gmail.com>
2021-04-05 21:39:58 +0530

    Update CTDeviceInfo.m

--- CleverTapSDK/CTDeviceInfo.m
+++ CleverTapSDK/CTDeviceInfo.m
@@ -142,13 +142,7 @@ static void CleverTapReachabilityHandler(SCNetworkReachabilityRef target, SCNetw
     struct utsname systemInfo;
     uname(&systemInfo);
     
-    NSString *platform = [NSString stringWithCString:systemInfo.machine
-                                            encoding:NSUTF8StringEncoding];
-    if (!platform || platform.length <= 0) {
-        CleverTapLogStaticDebug(@"Failed to fetch platform name from utsname.");
-        return nil;
-    }
-    return platform;
+    return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
 - (void)initDeviceID:(NSString *)cleverTapID {
