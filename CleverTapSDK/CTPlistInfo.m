468574ce647480e10bbba8c6850ef7609fda0e06
Akash Malhotra <akash.malhotra@clevertap.com>
2021-04-13 22:01:52 +0530

    1. Renamed a constant for IDFV Disabled key. 2. Removed an additional check inside CTPlistInfo

--- CleverTapSDK/CTPlistInfo.m
+++ CleverTapSDK/CTPlistInfo.m
@@ -87,10 +87,10 @@ static NSArray *registeredURLSchemes;
         
         
         // 1. FETCH IDFV FLAG FROM INFO.PLIST
-        NSString *disableIDFV = [CTPlistInfo getMetaDataForAttribute:CLTAP_USE_IDFV_LABEL];
+        NSString *disableIDFV = [CTPlistInfo getMetaDataForAttribute:CLTAP_DISABLE_IDFV_LABEL];
         
         // 2. IF NOTHING FOUND, DEFAULT TO YES
-        _disableIDFV = disableIDFV ? (disableIDFV && [disableIDFV isEqualToString:@"1"]) : NO;
+        _disableIDFV = disableIDFV ? ([disableIDFV isEqualToString:@"1"]) : NO;
     }
     return self;
 }
