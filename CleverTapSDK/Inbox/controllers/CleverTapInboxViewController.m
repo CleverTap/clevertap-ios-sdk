e427b82a97ced1d544c4069c1767056b9ec429ba
Akash Malhotra <akash.malhotra@clevertap.com>
2021-04-16 19:21:45 +0530

    renamed "title" to "firstTabTitle" at App Inbox VC.

--- CleverTapSDK/Inbox/controllers/CleverTapInboxViewController.m
+++ CleverTapSDK/Inbox/controllers/CleverTapInboxViewController.m
@@ -72,9 +72,9 @@ static const int kMaxTags = 3;
         
         if ([tags count] > 0) {
             // USE THE FIRST TAB TITLE PROVIDED IN THE CONFIG IF IT EXISTS, OR THE DEFAULT ONE
-            NSString *title = (config.firstTabTitle && config.firstTabTitle.length > 0) ? config.firstTabTitle : kDefaultTag;
+            NSString *firstTabTitle = (config.firstTabTitle && config.firstTabTitle.length > 0) ? config.firstTabTitle : kDefaultTag;
             
-            [tags insertObject:title atIndex:0];
+            [tags insertObject:firstTabTitle atIndex:0];
             _topContentOffset = 33.f;
         }
         if ([tags count] > kMaxTags) {
