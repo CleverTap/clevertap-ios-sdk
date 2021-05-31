1d85fe922df67091628c4832325c8ad7fb2fd9b9
adithyareddyZ <49646457+adithyareddyZ@users.noreply.github.com>
2021-05-31 12:08:28 +0530

    device/platform name fetch crash fix

--- CleverTapSDK/CTDeviceInfo.m
+++ CleverTapSDK/CTDeviceInfo.m
@@ -142,7 +142,75 @@ static void CleverTapReachabilityHandler(SCNetworkReachabilityRef target, SCNetw
     struct utsname systemInfo;
     uname(&systemInfo);
     
-    return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    NSString* code = [NSString stringWithCString:systemInfo.machine
+                                        encoding:NSUTF8StringEncoding];
+    
+    if (!deviceNamesByCode) {
+        deviceNamesByCode = @{@"i386" : @"iPhone Simulator",
+                              @"x86_64" : @"iPhone Simulator",
+                              @"iPhone1,1" : @"iPhone",
+                              @"iPhone1,2" : @"iPhone 3G",
+                              @"iPhone2,1" : @"iPhone 3GS",
+                              @"iPhone3,1" : @"iPhone 4",
+                              @"iPhone3,2" : @"iPhone 4 GSM Rev A",
+                              @"iPhone3,3" : @"iPhone 4 CDMA",
+                              @"iPhone4,1" : @"iPhone 4S",
+                              @"iPhone5,1" : @"iPhone 5 (GSM)",
+                              @"iPhone5,2" : @"iPhone 5 (GSM+CDMA)",
+                              @"iPhone5,3" : @"iPhone 5C (GSM)",
+                              @"iPhone5,4" : @"iPhone 5C (Global)",
+                              @"iPhone6,1" : @"iPhone 5S (GSM)",
+                              @"iPhone6,2" : @"iPhone 5S (Global)",
+                              @"iPhone7,1" : @"iPhone 6 Plus",
+                              @"iPhone7,2" : @"iPhone 6",
+                              @"iPhone8,1" : @"iPhone 6s",
+                              @"iPhone8,2" : @"iPhone 6s Plus",
+                              @"iPhone8,4" : @"iPhone SE (GSM)",
+                              @"iPhone9,1" : @"iPhone 7",
+                              @"iPhone9,2" : @"iPhone 7 Plus",
+                              @"iPhone9,3" : @"iPhone 7",
+                              @"iPhone9,4" : @"iPhone 7 Plus",
+                              @"iPhone10,1" : @"iPhone 8",
+                              @"iPhone10,2" : @"iPhone 8 Plus",
+                              @"iPhone10,3" : @"iPhone X Global",
+                              @"iPhone10,4" : @"iPhone 8",
+                              @"iPhone10,5" : @"iPhone 8 Plus",
+                              @"iPhone10,6" : @"iPhone X GSM",
+                              @"iPhone11,2" : @"iPhone XS",
+                              @"iPhone11,4" : @"iPhone XS Max",
+                              @"iPhone11,6" : @"iPhone XS Max Global",
+                              @"iPhone11,8" : @"iPhone XR",
+                              @"iPhone12,1" : @"iPhone 11",
+                              @"iPhone12,3" : @"iPhone 11 Pro",
+                              @"iPhone12,5" : @"iPhone 11 Pro Max",
+                              @"iPhone12,8" : @"iPhone SE 2nd Gen",
+                              @"iPhone13,1" : @"iPhone 12 Mini",
+                              @"iPhone13,2" : @"iPhone 12",
+                              @"iPhone13,3" : @"iPhone 12 Pro",
+                              @"iPhone13,4" : @"iPhone 12 Pro Max"
+                              };
+    }
+    
+    NSString* deviceName = [deviceNamesByCode objectForKey:code];
+    
+    if (!deviceName) {
+        // Not found on database. At least guess main device type from string contents:
+        
+        if ([code rangeOfString:@"iPod"].location != NSNotFound) {
+            deviceName = @"iPod Touch";
+        }
+        else if([code rangeOfString:@"iPad"].location != NSNotFound) {
+            deviceName = @"iPad";
+        }
+        else if([code rangeOfString:@"iPhone"].location != NSNotFound){
+            deviceName = @"iPhone";
+        }
+        else {
+            deviceName = code;
+        }
+    }
+    
+    return deviceName;
 }
 
 - (void)initDeviceID:(NSString *)cleverTapID {
