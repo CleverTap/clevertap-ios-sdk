2833280a795a9d2ed914c6a23acc07f7c47a328f
Aditi3 <aditi.gvc@gmail.com>
2021-06-16 13:15:08 +0530

    task: Add Unit Testing Target and Structure

---
+++ CleverTapSDKTests/SupportingFiles/CleverTapSDKTests-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "OCSwiftMock.h"
+#import "CTPlistInfo.h"
+#import "CTPreferences.h"
+#import "CleverTap+Tests.h"
+#import "CTCertificatePinning.h"
+#import "CTFeatureFlagsController.h"
---
+++ Podfile
@@ -0,0 +1,15 @@
+## Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+use_frameworks!
+
+def testing_pods
+  pod 'Quick'
+  pod 'Nimble'
+  pod 'OCMock'
+end
+
+target 'CleverTapSDKTests' do
+  inherit! :search_paths
+  testing_pods
+end
