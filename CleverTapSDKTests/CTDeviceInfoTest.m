27a82f5aace9f5ebfb269801b0828183c57f4174
Aishwarya Nanna <aishwarya.nanna@clevertap.com>
2023-03-08 13:33:33 +0530

    Unit tests for CTUtils class

---
+++ CleverTapSDKTests/CTDeviceInfoTest.m
@@ -0,0 +1,37 @@
+//
+//  CTDeviceInfoTest.m
+//  CleverTapSDKTests
+//
+//  Created by Aishwarya Nanna on 17/02/23.
+//  Copyright © 2023 CleverTap. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CTDeviceInfoTest : XCTestCase
+
+@end
+
+@implementation CTDeviceInfoTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
