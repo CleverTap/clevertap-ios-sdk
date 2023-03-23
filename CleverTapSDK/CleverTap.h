93ca2bad6ace39d9296d15be4f3b60cd2cda3089
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2023-03-23 19:25:55 +0200

    Allow calling FCU without block

--- CleverTapSDK/CleverTap.h
+++ CleverTapSDK/CleverTap.h
@@ -1341,7 +1341,7 @@ extern NSString * _Nonnull const CleverTapProfileDidInitializeNotification;
  
  @param block a callback with a boolean flag whether the update was successful.
  */
-- (void)forceContentUpdateWithBlock:(CleverTapForceContentUpdateBlock _Nonnull)block;
+- (void)forceContentUpdate:(CleverTapForceContentUpdateBlock _Nullable)block;
 
 @end
 
