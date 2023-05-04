31ce199ca9def80cea42927e5595378e1c2dcf6e
Aishwarya Nanna <aishwarya.nanna@clevertap.com>
2023-05-04 14:14:21 +0530

    Unit tests

---
+++ CleverTapSDKTests/CTEventBuilderTest.m
@@ -0,0 +1,37 @@
+//
+//  CTEventBuilderTest.m
+//  CleverTapSDKTests
+//
+//  Created by Aishwarya Nanna on 27/04/23.
+//  Copyright © 2023 CleverTap. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CTEventBuilderTest : XCTestCase
+
+@end
+
+@implementation CTEventBuilderTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
