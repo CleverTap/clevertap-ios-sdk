ee937eb74bc625c8874b50bb723349a780571ee6
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2023-11-17 18:48:08 +0200

    Add EvaluationManager tests

--- CleverTapSDKTests/InApps/CTInAppEvaluationManagerTest.m
+++ CleverTapSDKTests/InApps/CTInAppEvaluationManagerTest.m
@@ -18,19 +18,53 @@
 #import "InAppHelper.h"
 #import "CTConstants.h"
 
+@interface CTInAppStore(Tests)
+@property (nonatomic, strong) NSArray *serverSideInApps;
+@property (nonatomic, strong) NSArray *clientSideInApps;
+@end
+
 @interface CTInAppEvaluationManager(Test)
 @property (nonatomic, strong) CTInAppTriggerManager *triggerManager;
+@property (nonatomic, strong) CTInAppDisplayManager *inAppDisplayManager;
 @property (nonatomic, strong) NSMutableArray *suppressedClientSideInApps;
+@property (nonatomic, strong) NSMutableArray *evaluatedServerSideInAppIds;
 - (void)sortByPriority:(NSMutableArray *)inApps;
 - (NSMutableArray *)evaluate:(CTEventAdapter *)event withInApps:(NSArray *)inApps;
 - (BOOL)shouldSuppress:(NSDictionary *)inApp;
 - (void)suppress:(NSDictionary *)inApp;
 - (NSString *)generateWzrkId:(NSString *)ti;
 - (void)updateTTL:(NSMutableDictionary *)inApp;
+- (void)onAppLaunchedWithSuccess:(BOOL)success;
 @end
+
+@interface CTInAppDisplayManagerMock : CTInAppDisplayManager
+@property (nonatomic, strong) NSMutableArray *inappNotifs;
+- (void)_addInAppNotificationsToQueue:(NSArray *)inappNotifs;
+@end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wnonnull"
+@implementation CTInAppDisplayManagerMock
+- (instancetype)initWithNil {
+    if (self = [super initWithCleverTap:nil 
+                   dispatchQueueManager:nil
+                         inAppFCManager:nil
+                      impressionManager:nil
+                             inAppStore:nil]) {
+        self.inappNotifs = [NSMutableArray new];
+    }
+    return self;
+}
+- (void)_addInAppNotificationsToQueue:(NSArray *)inappNotifs {
+    [self.inappNotifs addObjectsFromArray:inappNotifs];
+}
+@end
+#pragma clang diagnostic pop
 
 @interface CTInAppEvaluationManagerTest : XCTestCase
 @property (nonatomic, strong) CTInAppEvaluationManager *evaluationManager;
+@property (nonatomic, strong) InAppHelper *helper;
+@property (nonatomic, strong) CTInAppDisplayManagerMock *mockDisplayManager;
 @end
 
 @implementation CTInAppEvaluationManagerTest
@@ -38,14 +72,16 @@
 - (void)setUp {
     [super setUp];
     
-    InAppHelper *helper = [InAppHelper new];
-    self.evaluationManager = helper.inAppEvaluationManager;
+    self.helper = [InAppHelper new];
+    self.mockDisplayManager = [[CTInAppDisplayManagerMock alloc] initWithNil];
+    self.evaluationManager = self.helper.inAppEvaluationManager;
+    self.evaluationManager.inAppDisplayManager = self.mockDisplayManager;
 }
 
 - (void)tearDown {
     // Clean up resources if needed
     //self.evaluationManager = nil;
-    for (int i = 1; i <= 3; i++) {
+    for (int i = 1; i <= 4; i++) {
         [self.evaluationManager.triggerManager removeTriggers:[NSString stringWithFormat:@"%d", i]];
     }
     [super tearDown];
@@ -193,7 +229,7 @@
 - (void)testEvaluateWithInApps {
     NSArray *inApps = @[
         @{
-            @"ti": @"1",
+            @"ti": @1,
             @"priority": @(100),
             @"whenTriggers": @[@{
                 @"eventName": @"event1",
@@ -216,7 +252,7 @@
             ]
         },
         @{
-            @"ti": @"2",
+            @"ti": @2,
             @"priority": @(100),
             @"whenTriggers": @[@{
                 @"eventName": @"event1",
@@ -229,10 +265,17 @@
             }]
         },
         @{
-            @"ti": @"3",
+            @"ti": @3,
             @"priority": @(100),
             @"whenTriggers": @[@{
                 @"eventName": @"event2"
+            }]
+        },
+        @{
+            @"ti": @4,
+            @"priority": @(100),
+            @"whenTriggers": @[@{
+                @"eventName": @"Charged"
             }]
         }
     ];
@@ -247,6 +290,279 @@
     XCTAssertEqual([self.evaluationManager.triggerManager getTriggers:@"1"], 2);
     XCTAssertEqual([self.evaluationManager.triggerManager getTriggers:@"2"], 2);
     XCTAssertEqual([self.evaluationManager.triggerManager getTriggers:@"3"], 1);
+    
+    // Charged
+    CTEventAdapter *eventCharged = [[CTEventAdapter alloc] initWithEventName:CLTAP_CHARGED_EVENT eventProperties:@{} location:kCLLocationCoordinate2DInvalid andItems:@[]];
+    
+    XCTAssertEqualObjects([self.evaluationManager evaluate:eventCharged withInApps:inApps], @[inApps[3]]);
+    XCTAssertEqual([self.evaluationManager.triggerManager getTriggers:@"4"], 1);
+}
+
+- (void)testEvaluateCharged {
+    self.helper.inAppStore.serverSideInApps = @[
+    @{
+        @"ti": @1,
+        @"whenTriggers": @[@{
+            @"eventName": @"event1"
+        }]
+    },
+    @{
+        @"ti": @2,
+        @"whenTriggers": @[@{
+            @"eventName": @"Charged"
+        }]
+    }];
+    [self.evaluationManager evaluateOnChargedEvent:@{} andItems:@[]];
+    XCTAssertEqualObjects((@[@2]), self.evaluationManager.evaluatedServerSideInAppIds);
+}
+
+- (void)testEvaluateServerSide {
+    self.helper.inAppStore.serverSideInApps = @[
+    @{
+        @"ti": @1,
+        @"whenTriggers": @[@{
+            @"eventName": @"event1"
+        }]
+    },
+    @{
+        @"ti": @2,
+        @"whenTriggers": @[@{
+            @"eventName": @"event1"
+        }]
+    },
+    @{
+        @"ti": @3,
+        @"whenTriggers": @[@{
+            @"eventName": @"event2"
+        }]
+    }];
+    [self.evaluationManager evaluateOnEvent:@"event1" withProps:@{}];
+    XCTAssertEqualObjects((@[@1, @2]), self.evaluationManager.evaluatedServerSideInAppIds);
+    [self.evaluationManager evaluateOnEvent:@"event2" withProps:@{}];
+    XCTAssertEqualObjects((@[@1, @2, @3]), self.evaluationManager.evaluatedServerSideInAppIds);
+}
+
+- (void)testEvaluateClientSide {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"whenTriggers": @[@{
+                @"eventName": @"event1"
+            }]
+        },
+        @{
+            @"ti": @2,
+            @"whenTriggers": @[@{
+                @"eventName": @"event1"
+            }]
+        },
+        @{
+            @"ti": @3,
+            @"whenTriggers": @[@{
+                @"eventName": @"event2"
+            }]
+        }];
+    self.helper.inAppStore.clientSideInApps = inApps;
+    
+    [self.evaluationManager evaluateOnEvent:@"event1" withProps:@{}];
+    // Add only one based on priority
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+    [self.evaluationManager evaluateOnEvent:@"event2" withProps:@{}];
+    XCTAssertEqualObjects((@[inApps[0], inApps[2]]), self.mockDisplayManager.inappNotifs);
+}
+
+- (void)testEvaluateClientSideSuppressed {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"suppressed": @YES,
+            @"whenTriggers": @[@{
+                @"eventName": @"event1"
+            }]
+        },
+        @{
+            @"ti": @2,
+            @"suppressed": @YES,
+            @"whenTriggers": @[@{
+                @"eventName": @"event1"
+            }]
+        },
+        @{
+            @"ti": @3,
+            @"whenTriggers": @[@{
+                @"eventName": @"event1"
+            }]
+        }];
+    self.helper.inAppStore.clientSideInApps = inApps;
+    
+    [self.evaluationManager evaluateOnEvent:@"event1" withProps:@{}];
+    // Suppress all until an in-app can be displayed
+    XCTAssertEqualObjects((@[inApps[2]]), self.mockDisplayManager.inappNotifs);
+    XCTAssertEqual(2, [self.evaluationManager.suppressedClientSideInApps count]);
+}
+
+- (void)testEvaluateOnAppLaunchedWithSuccess {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        }];
+    self.helper.inAppStore.clientSideInApps = inApps;
+    
+    [self.evaluationManager onAppLaunchedWithSuccess:YES];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+}
+
+- (void)testEvaluateOnAppLaunchedWithFailure {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        }];
+    self.helper.inAppStore.clientSideInApps = inApps;
+    // In-app is added on failure
+    [self.evaluationManager onAppLaunchedWithSuccess:NO];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+    
+    // No in-app is added on retry
+    [self.evaluationManager onAppLaunchedWithSuccess:NO];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+    
+    // No in-app is added on retry
+    [self.evaluationManager onAppLaunchedWithSuccess:YES];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+    
+    // Reset notifs
+    self.mockDisplayManager.inappNotifs = [NSMutableArray new];
+    
+    // In-app is added again on success
+    [self.evaluationManager onAppLaunchedWithSuccess:YES];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+    
+    // Reset notifs
+    self.mockDisplayManager.inappNotifs = [NSMutableArray new];
+    
+    // In-app is added again on failure
+    [self.evaluationManager onAppLaunchedWithSuccess:NO];
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+}
+
+- (void)testEvaluateOnAppLaunchedClientSide {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        },
+        @{
+            @"ti": @2,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        }];
+    self.helper.inAppStore.clientSideInApps = inApps;
+    
+    [self.evaluationManager evaluateOnAppLaunchedClientSide];
+    // Add only one based on priority
+    XCTAssertEqualObjects((@[inApps[0]]), self.mockDisplayManager.inappNotifs);
+}
+
+- (void)testEvaluateOnAppLaunchedServerSide {
+    NSArray *inApps = @[
+        @{
+            @"ti": @1,
+            @"suppressed": @YES,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        },
+        @{
+            @"ti": @2,
+            @"suppressed": @YES,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        },
+        @{
+            @"ti": @3,
+            @"whenTriggers": @[@{
+                @"eventName": @"App Launched"
+            }]
+        }];
+    
+    [self.evaluationManager evaluateOnAppLaunchedServerSide:inApps];
+    // Suppress all until an in-app can be displayed
+    XCTAssertEqualObjects((@[inApps[2]]), self.mockDisplayManager.inappNotifs);
+    XCTAssertEqual(2, [self.evaluationManager.suppressedClientSideInApps count]);
+}
+
+- (void)testOnBatchHeaderCreation {
+    self.evaluationManager.evaluatedServerSideInAppIds = [@[@1, @2, @3] mutableCopy];
+    self.evaluationManager.suppressedClientSideInApps = [@[@4, @5, @6] mutableCopy];
+    NSMutableDictionary *expected = [NSMutableDictionary new];
+    expected[CLTAP_INAPP_SS_EVAL_META_KEY] = @[@1, @2, @3];
+    expected[CLTAP_INAPP_SUPPRESSED_META_KEY] = @[@4, @5, @6];
+    
+    NSDictionary *batchHeaderKVO = [self.evaluationManager onBatchHeaderCreationForQueue:CTQueueTypeEvents];
+    XCTAssertEqualObjects(expected, batchHeaderKVO);
+    
+    NSDictionary *batchHeaderKVOProfile = [self.evaluationManager onBatchHeaderCreationForQueue:CTQueueTypeProfile];
+    XCTAssertEqualObjects(@{}, batchHeaderKVOProfile);
+}
+
+- (void)testOnBatchSentRemoveAll {
+    self.evaluationManager.evaluatedServerSideInAppIds = [@[@1, @2, @3] mutableCopy];
+    self.evaluationManager.suppressedClientSideInApps = [@[@4, @5, @6] mutableCopy];
+    NSArray *batchWithHeaderAll = @[
+        @{
+            CLTAP_INAPP_SS_EVAL_META_KEY: @[@1, @2, @3],
+            CLTAP_INAPP_SUPPRESSED_META_KEY: @[@4, @5, @6]
+        }
+    ];
+    [self.evaluationManager onBatchSent:batchWithHeaderAll withSuccess:NO];
+    XCTAssertEqualObjects((@[@1, @2, @3]), self.evaluationManager.evaluatedServerSideInAppIds);
+    XCTAssertEqualObjects((@[@4, @5, @6]), self.evaluationManager.suppressedClientSideInApps);
+
+    [self.evaluationManager onBatchSent:batchWithHeaderAll withSuccess:YES];
+    XCTAssertEqualObjects((@[]), self.evaluationManager.evaluatedServerSideInAppIds);
+    XCTAssertEqualObjects((@[]), self.evaluationManager.suppressedClientSideInApps);
+}
+
+- (void)testOnBatchSentRemoveElements {
+    self.evaluationManager.evaluatedServerSideInAppIds = [@[@1, @2, @3] mutableCopy];
+    self.evaluationManager.suppressedClientSideInApps = [@[@4, @5, @6] mutableCopy];
+    NSArray *batchWithHeader = @[
+        @{
+            CLTAP_INAPP_SS_EVAL_META_KEY: @[@1, @2],
+            CLTAP_INAPP_SUPPRESSED_META_KEY: @[@4]
+        }
+    ];
+    NSArray *batchWithHeaderAll = @[
+        @{
+            CLTAP_INAPP_SS_EVAL_META_KEY: @[@1, @2, @3],
+            CLTAP_INAPP_SUPPRESSED_META_KEY: @[@4, @5, @6]
+        }
+    ];
+    // If batch is not successful, do not remove elements
+    [self.evaluationManager onBatchSent:batchWithHeader withSuccess:NO];
+    XCTAssertEqualObjects((@[@1, @2, @3]), self.evaluationManager.evaluatedServerSideInAppIds);
+    XCTAssertEqualObjects((@[@4, @5, @6]), self.evaluationManager.suppressedClientSideInApps);
+
+    // Remove only the first n elements in the batch
+    [self.evaluationManager onBatchSent:batchWithHeader withSuccess:YES];
+    XCTAssertEqualObjects((@[@3]), self.evaluationManager.evaluatedServerSideInAppIds);
+    XCTAssertEqualObjects((@[@5, @6]), self.evaluationManager.suppressedClientSideInApps);
+    
+    // Remove all elements, ensure no out of range exception
+    // Current values are @[@3] and @[@5, @6]
+    [self.evaluationManager onBatchSent:batchWithHeaderAll withSuccess:YES];
+    XCTAssertEqualObjects((@[]), self.evaluationManager.evaluatedServerSideInAppIds);
+    XCTAssertEqualObjects((@[]), self.evaluationManager.suppressedClientSideInApps);
 }
 
 - (void)testShouldSuppress {
