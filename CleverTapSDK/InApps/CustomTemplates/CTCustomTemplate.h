e390c39501054802d84f0eb641a25900ca53ad31
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2024-10-01 22:50:37 +0300

    Add code documentation.
    @throws breaks Xcode quick help, so exceptions info is added to Discussion

--- CleverTapSDK/InApps/CustomTemplates/CTCustomTemplate.h
+++ CleverTapSDK/InApps/CustomTemplates/CTCustomTemplate.h
@@ -10,9 +10,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/*!
+ A definition of a custom template. Can be a function or a code template.
+ Instances are uniquely identified by their name.
+ */
 @interface CTCustomTemplate : NSObject
 
+/*!
+ The name of the template.
+ */
 @property (nonatomic, strong, readonly) NSString *name;
+
+/*!
+ Whether the template has UI or not.
+ */
 @property (nonatomic, readonly) BOOL isVisual;
 
 - (instancetype)init NS_UNAVAILABLE;
