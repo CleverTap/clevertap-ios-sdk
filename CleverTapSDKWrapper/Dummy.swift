21012b9ad3381902f0680610f0508c32fa27df60
Akash Malhotra <carljohnson.akash@gmail.com>
2025-04-09 10:51:47 +0530

    removed placeholder.h. removed methods in dummy.swift. added -destination to the update package.swift workflow

--- CleverTapSDKWrapper/Dummy.swift
+++ CleverTapSDKWrapper/Dummy.swift
@@ -1,7 +1,4 @@
 // Dummy.swift
 // This file is required for SPM to recognize the target
 public struct CleverTapSDKWrapper {
-    public static func initialize() {
-        print("CleverTap SDK Wrapper initialized")
-    }
 }
--- CleverTapSDKWrapper/placeholder.h
+++
@@ -1,7 +0,0 @@
-//
-//  placeholder.h
-//  CleverTapSDK
-//
-//  Created by Akash Malhotra on 27/02/25.
-//
-
