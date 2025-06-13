b8a8f98d7bcdd3bedd6d5b173ca03d84b085c894
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2025-06-13 13:32:36 +0300

    Fix function present and dismiss

--- SwiftUIStarter/SwiftUIStarter/CustomTemplates/CTCopyToClipBoardPresenter.swift
+++ SwiftUIStarter/SwiftUIStarter/CustomTemplates/CTCopyToClipBoardPresenter.swift
@@ -5,13 +5,19 @@ import SwiftUI
 class CTCopyToClipBoardPresenter: CTTemplatePresenter {
     
     public func onPresent(context: CTTemplateContext) {
-        let text = context.string(name: CTCopyToClipboardTemplate.ArgumentNames.text)
-        if let text = text {
-            UIPasteboard.general.string = text
+        guard let text = context.string(name: CTCopyToClipboardTemplate.ArgumentNames.text),
+                !text.isEmpty else {
+            print("\(self): Text argument missing")
+            return
         }
+        
+        UIPasteboard.general.string = text
+        context.presented()
+        context.dismissed()
     }
     
     func onCloseClicked(context: CTTemplateContext) {
+        // NOOP: Non-visual function without actions
     }
 }
 
