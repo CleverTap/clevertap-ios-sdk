4f7fe2e8abc51c1fa89b420c4416a7c504c56b40
ReshabCT <reshab@clevertap.com>
2026-04-27 15:39:17 +0530

    SDK-5642: Removed NSlogs from alert inapp for tvOS

--- CleverTapSDK/InApps/CTAlertViewController.m
+++ CleverTapSDK/InApps/CTAlertViewController.m
@@ -98,7 +98,6 @@
     if (!self.notification) return;
 
 #if TARGET_OS_TV
-    NSLog(@"[CT-tvOS] showFromWindow: tvOS path entered, notification title: %@", self.notification.title);
     [self showAlertFromTopViewController];
 #else
     if (@available(iOS 13, *)) {
@@ -134,16 +133,12 @@
 
 #if TARGET_OS_TV
 - (void)showAlertFromTopViewController {
-    NSLog(@"[CT-tvOS] showAlertFromTopViewController called, notification: %@", self.notification.title);
 
     UIViewController *topVC = [self topViewController];
-    NSLog(@"[CT-tvOS] topViewController: %@", topVC);
     if (!topVC) {
-        NSLog(@"[CT-tvOS] ERROR: No topViewController found, cannot present alert");
         return;
     }
 
-    NSLog(@"[CT-tvOS] Creating UIAlertController with title: %@, message: %@",
           self.notification.title, self.notification.message);
 
     UIAlertController *dialogBox = [UIAlertController
@@ -152,7 +147,6 @@
                                     preferredStyle:UIAlertControllerStyleAlert];
 
     if (self.notification.buttons && self.notification.buttons.count > 0) {
-        NSLog(@"[CT-tvOS] Adding %lu buttons", (unsigned long)self.notification.buttons.count);
         for (NSUInteger i = 0; i < self.notification.buttons.count && i < 3; i++) {
             NSUInteger index = i;
             UIAlertAction *button = [UIAlertAction
@@ -171,7 +165,6 @@
         [self.delegate notificationDidShow:self.notification];
     }
 
-    NSLog(@"[CT-tvOS] Presenting alert on: %@", topVC);
     [topVC presentViewController:dialogBox animated:YES completion:^{
         NSLog(@"[CT-tvOS] Alert presentation completed");
     }];
@@ -184,20 +177,14 @@
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
     keyWindow = [CTUIUtils getSharedApplication].keyWindow;
 #pragma clang diagnostic pop
-    NSLog(@"[CT-tvOS] keyWindow from UIApplication: %@", keyWindow);
 
     if (!keyWindow) {
         if (@available(tvOS 13.0, *)) {
             NSSet *scenes = [CTUIUtils getSharedApplication].connectedScenes;
-            NSLog(@"[CT-tvOS] connectedScenes count: %lu", (unsigned long)scenes.count);
             for (UIScene *scene in scenes) {
-                NSLog(@"[CT-tvOS] scene: %@, state: %ld, isKindOfWindowScene: %d",
-                      scene, (long)scene.activationState, [scene isKindOfClass:[UIWindowScene class]]);
                 if ([scene isKindOfClass:[UIWindowScene class]]) {
                     UIWindowScene *windowScene = (UIWindowScene *)scene;
-                    NSLog(@"[CT-tvOS] windowScene.windows count: %lu", (unsigned long)windowScene.windows.count);
                     for (UIWindow *window in windowScene.windows) {
-                        NSLog(@"[CT-tvOS] window: %@, isKey: %d", window, window.isKeyWindow);
                         if (window.isKeyWindow) {
                             keyWindow = window;
                             break;
@@ -210,12 +197,10 @@
     }
 
     if (!keyWindow) {
-        NSLog(@"[CT-tvOS] ERROR: No keyWindow found at all");
         return nil;
     }
 
     UIViewController *topVC = keyWindow.rootViewController;
-    NSLog(@"[CT-tvOS] rootViewController: %@, presentedVC: %@", topVC, topVC.presentedViewController);
     while (topVC.presentedViewController) {
         topVC = topVC.presentedViewController;
     }
