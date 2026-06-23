b2c1d171583431f745c474341f9a720d7e696b14
ReshabCT <reshab@clevertap.com>
2026-06-23 17:00:24 +0530

    Fix all tvOS xibs and add guards in interstitial vc for tvOS support

--- CleverTapSDK/InApps/CTInterstitialViewController.m
+++ CleverTapSDK/InApps/CTInterstitialViewController.m
@@ -5,10 +5,8 @@
 #import "CTDismissButton.h"
 #import "CTInAppUtils.h"
 #import "CTAVPlayerViewController.h"
-#if !(TARGET_OS_TV)
 #import <SDWebImage/UIImageView+WebCache.h>
 #import <SDWebImage/SDAnimatedImageView+WebCache.h>
-#endif
 
 #import <AVFoundation/AVFoundation.h>
 #import <AVKit/AVKit.h>
@@ -18,11 +16,7 @@
 @property (nonatomic, strong) IBOutlet UIView *containerView;
 @property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property (nonatomic, strong) IBOutlet UILabel *bodyLabel;
-#if !(TARGET_OS_TV)
 @property (nonatomic, strong) IBOutlet SDAnimatedImageView *imageView;
-#else
-@property (nonatomic, strong) IBOutlet UIImageView *imageView;
-#endif
 @property (nonatomic, strong) IBOutlet UIView *avPlayerContainerView;
 @property (nonatomic, strong) IBOutlet UIView *buttonsContainer;
 @property (nonatomic, strong) IBOutlet UIView *secondButtonContainer;
@@ -329,16 +323,12 @@
         self.imageView.accessibilityLabel = self.notification.contentDescription;
     } else if (self.notification.imageData) {
         self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-#if !(TARGET_OS_TV)
-        if ([self.notification.contentType isEqualToString:@"image/gif"] ) {
+        if ([self.notification.contentType isEqualToString:@"image/gif"]) {
             SDAnimatedImage *gif = [SDAnimatedImage imageWithData:self.notification.imageData];
             self.imageView.image = gif;
         } else {
             self.imageView.image = [UIImage imageWithData:self.notification.imageData];
         }
-#else
-        self.imageView.image = [UIImage imageWithData:self.notification.imageData];
-#endif
         self.imageView.accessibilityLabel = self.notification.contentDescription;
     }
     
