c6cececa1972a35dc60d4a3a8e2642933cd032de
ReshabCT <reshab@clevertap.com>
2026-06-29 17:01:32 +0530

    Add accessibility label for cancel button and increase 44pt tap area

--- CleverTapSDK/InApps/CTDismissButton.m
+++ CleverTapSDK/InApps/CTDismissButton.m
@@ -37,6 +37,13 @@ static UIImage *dismissButtonImage;
             dismissButtonImage = [self dismissButtonImage];
         });
     }
+    self.accessibilityLabel = @"Close";
+}
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    CGFloat expansion = MAX(0.0, (44.0 - self.bounds.size.width) / 2.0);
+    CGRect expandedBounds = CGRectInset(self.bounds, -expansion, -expansion);
+    return CGRectContainsPoint(expandedBounds, point);
 }
 
 - (UIImage *)dismissButtonImage {
