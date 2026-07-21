40afd80118c2fed7fa8802e9e9fab1a34f8cc0a3
ReshabCT <reshab@clevertap.com>
2026-07-21 22:10:03 +0530

    Fixed pr comment

--- CleverTapSDK/CTInAppDisplayViewControllerPrivate.h
+++ CleverTapSDK/CTInAppDisplayViewControllerPrivate.h
@@ -30,7 +30,6 @@
 - (void)handleImageTapGesture;
 - (UIButton*)setupViewForButton:(UIButton *)buttonView withData:(CTNotificationButton *)button withIndex:(NSInteger)index;
 
-- (UIView *)accessibilityFocusTarget;
 - (void)announceInAppShown;
 
 @end
