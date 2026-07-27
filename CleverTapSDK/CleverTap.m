b1d7ae340e547e170aee004ab9c8a1d9ea53b5f1
Akash Malhotra <carljohnson.akash@gmail.com>
2026-07-27 16:58:53 +0530

    Update CleverTap.m

--- CleverTapSDK/CleverTap.m
+++ CleverTapSDK/CleverTap.m
@@ -1183,7 +1183,7 @@ static BOOL sharedInstanceErrorLogged;
 #if !CLEVERTAP_NO_INAPP_SUPPORT
     if (isActuallyInForeground && !_config.analyticsOnly && ![CTUIUtils runningInsideAppExtension]) {
         [self.inAppFCManager checkUpdateDailyLimits];
-        // Show inapps that werent shown cuz of the app being in the background
+        // Show inapps that were not shown because of the app being in the background
         [self.inAppDisplayManager _showInAppNotificationIfAny];
     }
 #endif
